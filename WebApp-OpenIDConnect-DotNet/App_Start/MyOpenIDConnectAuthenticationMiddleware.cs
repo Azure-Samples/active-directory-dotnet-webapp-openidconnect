d8dbfff252134ed72556fa1cacd0682387cd0e82
Tushar Gupta <tugupta@microsoft.com>
2014-10-17 16:27:11 -0700

    Setting unique cookie for each authentication request

---
+++ WebApp-OpenIDConnect-DotNet/App_Start/MyOpenIDConnectAuthenticationMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Logging;
+using Microsoft.Owin.Security.Infrastructure;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Owin;
+
+namespace WebApp_OpenIDConnect_DotNet.Models
+{
+    public class MyOpenIDConnectAuthenticationMiddleware : OpenIdConnectAuthenticationMiddleware
+    {
+        private readonly ILogger _logger;
+
+        public MyOpenIDConnectAuthenticationMiddleware(OwinMiddleware next, IAppBuilder app, OpenIdConnectAuthenticationOptions options)
+            : base(next, app, options)
+        {
+            _logger = app.CreateLogger<MyOpenIDConnectAuthenticationMiddleware>();
+        }
+
+        protected override AuthenticationHandler<OpenIdConnectAuthenticationOptions> CreateHandler()
+        {
+            return new MyOpenIDConnectAuthenticationHandler(_logger);
+        }
+    }
+}
